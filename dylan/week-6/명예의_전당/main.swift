507b1add7ac3926da89013aeb92822f654ce74e6
DevWhkang <devwhkang@gmail.com>
2023-03-20 21:26:32 +0900

    Add. 명예의_전당 풀이

--- dylan/week-6/명예의_전당/main.swift
+++ dylan/week-6/명예의_전당/main.swift
@@ -7,5 +7,37 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ k:Int, _ score:[Int]) -> [Int] {
+    var priorityQueue: [Int] = Array(repeating: -1, count: k)
+    var result: [Int] = []
+    
+    func enqueueToPriorityQueue(_ grage: Int) {
+        priorityQueue[priorityQueue.count - 1] = grage
+        priorityQueue.sort(by: { next, prev in
+            return next > prev
+        })
+    }
+    
+    func getLowestInPriorityQueue() -> Int {
+        return priorityQueue.filter { grage in
+            return grage != -1
+        }.last!
+    }
+    
+    for (day, grade) in score.enumerated() {
+        if(day < k) {
+            enqueueToPriorityQueue(grade)
+        }
+        
+        if(day >= k && grade > priorityQueue.last!) {
+            enqueueToPriorityQueue(grade)
+        }
+        
+        result.append(getLowestInPriorityQueue())
+    }
+    
+    return result
+}
+
+print(solution(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]))
 
