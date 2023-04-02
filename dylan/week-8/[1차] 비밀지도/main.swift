fb4644601d89b9a0f2ddd43c1e508defb3d1090b
DevWhkang <devwhkang@gmail.com>
2023-04-02 19:13:46 +0900

    Add. [1차] 비밀지도

--- dylan/week-8/[1차] 비밀지도/main.swift
+++ dylan/week-8/[1차] 비밀지도/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    var answer: [String] = []
+    
+    func appendPadding(_ bin: String) -> String {
+        var copied = bin
+        
+        while(copied.count < n) {
+            copied = "0" + copied
+        }
+        
+        return copied
+    }
+    
+    for (index, ele) in arr1.enumerated() {
+        let addedBool = ele | arr2[index]
+        let binary =  String(addedBool, radix: 2)
+        let paddingBin = appendPadding(binary)
+            
+        let converted = paddingBin.replacingOccurrences(of: "1", with: "#").replacingOccurrences(of: "0", with: " ")
+        
+        answer.append(converted)
+    }
+    
+    return answer
+}
+
+print(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
 
