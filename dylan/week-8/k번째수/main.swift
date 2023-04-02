12a5d0130c6da3ae068c9ef6ace05289a36c3c86
DevWhkang <devwhkang@gmail.com>
2023-04-02 17:56:44 +0900

    Add. k번째수 풀이

--- dylan/week-8/k번째수/main.swift
+++ dylan/week-8/k번째수/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    var result: [Int] = []
+    
+    for arr in commands {
+        let startIndex = arr[0] - 1
+        let endIndex = arr[1] - 1
+        let nthIndex = arr[2] - 1
+        
+        let slice = array[startIndex...endIndex].sorted()
+        
+        result.append(slice[nthIndex])
+    }
+    
+    return result
+}
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
 
